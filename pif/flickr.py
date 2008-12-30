b9bfc930333ce82bd6114b71c8df34e5517cfb77
Scott Robinson <scott@quadhome.com>
2008-12-30 00:51:05 -0800

    Initial port of FlickrIndex from flickrsync. Full positive unit test coverage.

--- pif/flickr.py
+++ pif/flickr.py
@@ -1,7 +1,10 @@
+import hashlib
 import logging
+import threadpool
 import time
 
 import flickrapi
+import httplib2
 import pkg_resources
 
 API_KEY, API_SECRET = pkg_resources.resource_string(__name__, 'flickr-api.key').split()
@@ -10,7 +13,7 @@ def _default_proxy_cb():
     logging.info('Waiting for authorization from Flickr...')
     time.sleep(5)
 
-def get_proxy(key = API_KEY, secret = API_SECRET, wait_callback = _default_proxy_cb):
+def get_proxy(key=API_KEY, secret=API_SECRET, wait_callback=_default_proxy_cb):
     # Setup the API proxy.
     proxy = flickrapi.FlickrAPI(key, secret, format = 'etree')
 
@@ -29,4 +32,111 @@ def get_proxy(key = API_KEY, secret = API_SECRET, wait_callback = _default_proxy
 
     return proxy
 
+class FlickrPhoto:
+    TAILHASH_SIZE = 512
 
+    def __init__(self, id):
+        self.id = id
+
+    def _get_url(self):
+        return "http://farm%s.static.flickr.com/%s/%s_%s_o.%s" % \
+                (self.farm,
+                 self.server,
+                 self.id,
+                 self.original_secret,
+                 self.original_format)
+
+    url = property(_get_url)
+
+class FlickrIndex:
+    NUM_WORKERS = 4
+
+    def __init__(self, proxy):
+        self.proxy = proxy
+        self.last_update = 1
+        self.photos = {}
+
+    def refresh(self):
+        new_photos = self.refresh_updated()
+        self.refresh_sizes(new_photos)
+
+    def refresh_updated(self):
+        """Synchronize to the recently updated photos."""
+
+        def recent_pages(*args, **kwargs):
+            """An iterator of the recent updated photo pages."""
+
+            page, pages = 1, 1
+            while page <= pages:
+                photos = self.proxy.photos_recentlyUpdated(page=page, *args, **kwargs).find('photos')
+
+                if photos: yield photos
+                else: break
+
+                page = int(photos.get('page')) + 1
+                pages = int(photos.get('pages'))
+
+        # Pull down the index of updated photos.
+        photo_pages = recent_pages(extras='o_dims, original_format, last_update',
+                                   min_date=self.last_update)
+
+        # Create records for the photos.
+
+        new_photos = []
+
+        for photo_page_num, photo_page in enumerate(photo_pages):
+            for photo_xml in photo_page.findall('photo'):
+                pid = int(photo_xml.attrib['id'])
+                last_update = int(photo_xml.attrib['lastupdate'])
+
+                # Only update if there has been changes.
+                
+                if self.photos.has_key(pid):
+                    p = self.photos[pid]
+                    
+                    if p.last_update == last_update:
+                        continue
+                else:
+                    p = FlickrPhoto(pid)
+                    self.photos[p.id] = p
+
+                p.last_update = last_update
+
+                p.original_format = photo_xml.attrib['originalformat']
+                p.original_secret = photo_xml.attrib['originalsecret']
+
+                p.farm = int(photo_xml.attrib['farm'])
+                p.server = int(photo_xml.attrib['server'])
+
+                p.height = int(photo_xml.attrib['o_height'])
+                p.width = int(photo_xml.attrib['o_width'])
+
+                self.last_update = max(self.last_update, int(photo_xml.attrib['lastupdate']))
+
+                new_photos.append(p)
+
+        return new_photos
+
+    def refresh_sizes(self, new_photos):
+        def get_photo_size(photo):
+            # TODO: Persist the HTTP connection.
+            h = httplib2.Http()
+
+            resp, content = h.request(uri = photo.url,
+                                      headers = {'Range': "bytes=-%u" % FlickrPhoto.TAILHASH_SIZE})
+            assert resp.status == 206
+
+            photo.size = int(resp['content-range'].split('/')[-1])
+            photo.tailhash = hashlib.sha512(content).hexdigest()
+
+        requests = [threadpool.WorkRequest(get_photo_size, (p, ))
+                    for p in new_photos]
+
+        if requests:
+            pool = threadpool.ThreadPool(self.NUM_WORKERS)
+
+            for r in requests:
+                pool.putRequest(r)
+            pool.wait()
+
+            pool.dismissWorkers(self.NUM_WORKERS)
--- tests/flickr-tests.py
+++ tests/flickr-tests.py
@@ -1,43 +1,279 @@
+import hashlib
 import unittest
 
-import flickrapi
+from xml.etree.ElementTree import XML
 
+import flickrapi
+import minimock
+
+from minimock import Mock
 from nose.exc import SkipTest
 from nose.tools import raises
 
-from pif.flickr import get_proxy
+from pif.flickr import get_proxy, FlickrIndex
 
-class ProxyTests(unittest.TestCase):
+class OnlineProxyTests(unittest.TestCase):
     """Flickr proxy tests."""
+
+    def tearDown(self):
+        minimock.restore()
 
     @raises(flickrapi.FlickrError)
     def test_invalid_key(self):
+        """Proxy with invalid key"""
+
+        frob = Mock('frob')
+        frob.read.mock_returns = """<?xml version="1.0" encoding="utf-8" ?>
+<rsp stat="fail">
+    <err code="100" msg="Invalid API Key (Key not found)" />
+</rsp>"""
+
+        import urllib
+        minimock.mock('urllib.urlopen', returns=frob)
+
         get_proxy(key = 'xyzzy')
 
     @raises(flickrapi.FlickrError)
     def test_invalid_secret(self):
+        """Proxy with invalid secret"""
+
+        frob = Mock('frob')
+        frob.read.mock_returns = """<?xml version="1.0" encoding="utf-8" ?>
+<rsp stat="fail">
+    <err code="96" msg="Invalid signature" />
+</rsp>"""
+
+        import urllib
+        minimock.mock('urllib.urlopen', returns=frob)
+
         get_proxy(secret = 'xyzzy')
-
-    @raises(flickrapi.FlickrError)
-    def test_invalid_key_and_secret(self):
-        get_proxy('abc', '123')
-
-    @raises(flickrapi.FlickrError)
-    def test_reject(self):
-        raise SkipTest, "Flickr doesn't reject API requests?"
-
-    def test_ok(self):
-        self.hit_cb = False
-
-        def cb():
-            self.hit_cb = True
-
-        assert isinstance(get_proxy(wait_callback = cb), flickrapi.FlickrAPI)
-        assert self.hit_cb
 
     @raises(IOError)
     def test_offline(self):
-        raise SkipTest, "How can we simulate being offline?"
+        """Proxy when offline"""
 
-class FlickrIndexTests(unittest.TestCase):
-    """Flickr index tests."""
+        import urllib
+        minimock.mock('urllib.urlopen', raises=IOError)
+
+        get_proxy()
+
+    @raises(flickrapi.FlickrError)
+    def test_reject(self):
+        """Rejected proxy"""
+
+        minimock.mock('flickrapi.FlickrAPI.get_token_part_one')
+        minimock.mock('flickrapi.FlickrAPI.get_token_part_two', raises=flickrapi.FlickrError)
+
+        get_proxy()
+
+    def test_ok(self):
+        """OK proxy"""
+
+        minimock.mock('flickrapi.FlickrAPI.get_token_part_one')
+        minimock.mock('flickrapi.FlickrAPI.get_token_part_two', returns=True)
+
+        assert isinstance(get_proxy(), flickrapi.FlickrAPI)
+
+    def test_cb(self):
+        """Callback from proxy"""
+
+        minimock.mock('flickrapi.FlickrAPI.get_token_part_one')
+
+        def bad_api(auth_response):
+            raise flickrapi.FlickrError('Error: 108')
+
+        minimock.mock('flickrapi.FlickrAPI.get_token_part_two', returns_func=bad_api)
+
+        self.hit_cb = False
+
+        def cb():
+            if self.hit_cb:
+                return True
+            else:
+                self.hit_cb = True
+
+        self.assertRaises(flickrapi.FlickrError, get_proxy, wait_callback=cb)
+        assert self.hit_cb
+
+class IndexTests(unittest.TestCase):
+    def setUp(self):
+        self.proxy = Mock('FlickrAPI')
+        self.index = FlickrIndex(self.proxy)
+
+    def tearDown(self):
+        minimock.restore()
+
+    def test_empty(self):
+        """Empty Flickr index"""
+
+        assert self.index.last_update == 1
+        assert not self.index.photos
+
+    def test_refresh_fail_response(self):
+        """Failed refresh from Flickr"""
+
+        photos_xml = XML("""
+                         <rsp stat="fail">
+                            <err code="1234" msg="Test Message" />
+                         </rsp>""")
+        self.proxy.photos_recentlyUpdated.mock_returns = photos_xml
+
+        self.index.refresh()
+        assert not self.index.photos
+
+    XML_DATA = {
+        2717638353: {
+            'farm': 4,
+            'height': 1024,
+            'id': 2717638353,
+            'last_update': 1227123744,
+            'original_format': 'jpg',
+            'original_secret': '1111111111',
+            'server': 3071,
+            'size': 12345,
+            'width': 1544,
+        },
+
+        2658720703: {
+            'farm': 3,
+            'height': 1500,
+            'id': 2658720703,
+            'last_update': 1226520673,
+            'original_format': 'jpg',
+            'original_secret': 'yyyyyyyyyy',
+            'server': 2126,
+            'size': 53241,
+            'width': 1000,
+        },
+
+        2740209939: {
+            'farm': 4,
+            'height': 2736,
+            'id': 2740209939,
+            'last_update': 1222928301,
+            'original_format': 'jpg',
+            'original_secret': '3333333333',
+            'server': 3108,
+            'size': 98764,
+            'width': 3648,
+        }
+    }
+
+    def _mock_photos_http(self):
+        class MockResponse(dict):
+            def __init__(self, status, length):
+                self.status = status
+                self['content-range'] = '%u-%u/%u' % ((length - 513), (length - 1), length)
+
+        responses = []
+
+        for id, values in self.XML_DATA.iteritems():
+            data = ('*' * 512 + str(id))[:512]
+            values['tailhash'] = hashlib.sha512(data).hexdigest()
+
+            responses.append((MockResponse(206, values['size']), data))
+
+        import httplib2
+        minimock.mock('httplib2.Http.request', returns_iter=responses)
+    
+    def _check_photo_data(self, photos):
+        assert len(photos) == len(self.XML_DATA)
+
+        for id, values in self.XML_DATA.iteritems():
+            for name, result in values.iteritems():
+                assert getattr(photos[id], name) == result, "FlickrPhoto(%u).%s != %s (is %s)" % \
+                        (id, name, result, getattr(photos[id], name))
+
+    def test_refresh_empty_response(self):
+        """Empty response from Flickr"""
+
+        photos_xml = XML("""
+                        <rsp>
+                            <photos />
+                        </rsp>""")
+        self.proxy.photos_recentlyUpdated.mock_returns = photos_xml
+
+        self.index.refresh()
+        assert not self.index.photos
+
+    def test_refresh_single_page(self):
+        """Single page, multiple photos"""
+
+        photos_xml = XML("""
+                        <rsp>
+                            <photos page="1" pages="1" perpage="100" total="3">
+                                <photo farm="4" id="2717638353" isfamily="0" isfriend="0" ispublic="1" lastupdate="1227123744" o_height="1024" o_width="1544" originalformat="jpg" originalsecret="1111111111" owner="25046991@N00" secret="xxxxxxxxxx" server="3071" title="87680027.JPG" />
+                                <photo farm="4" id="2740209939" isfamily="0" isfriend="0" ispublic="1" lastupdate="1222928301" o_height="2736" o_width="3648" originalformat="jpg" originalsecret="3333333333" owner="25046991@N00" secret="zzzzzzzzzz" server="3108" title="IMG_0394.JPG" />
+                                <photo farm="3" id="2658720703" isfamily="0" isfriend="0" ispublic="1" lastupdate="1226520673" o_height="1500" o_width="1000" originalformat="jpg" originalsecret="yyyyyyyyyy" owner="25046991@N00" secret="2222222222" server="2126" title="16.jpg" />
+                            </photos>
+                        </rsp>""")
+        self.proxy.photos_recentlyUpdated.mock_returns = photos_xml
+
+        self._mock_photos_http()
+
+        self.index.refresh()
+
+        self._check_photo_data(self.index.photos)
+        assert self.index.last_update == 1227123744
+
+    def test_refresh_multi_page(self):
+        """Multiple pages, single photos"""
+
+        photos_xml_1 = XML("""
+                        <rsp>
+                            <photos page="1" pages="3" perpage="1" total="3">
+                                <photo farm="4" id="2717638353" isfamily="0" isfriend="0" ispublic="1" lastupdate="1227123744" o_height="1024" o_width="1544" originalformat="jpg" originalsecret="1111111111" owner="25046991@N00" secret="xxxxxxxxxx" server="3071" title="87680027.JPG" />
+                            </photos>
+                        </rsp>""")
+
+        photos_xml_2 = XML("""
+                        <rsp>
+                            <photos page="2" pages="3" perpage="1" total="3">
+                                <photo farm="4" id="2740209939" isfamily="0" isfriend="0" ispublic="1" lastupdate="1222928301" o_height="2736" o_width="3648" originalformat="jpg" originalsecret="3333333333" owner="25046991@N00" secret="zzzzzzzzzz" server="3108" title="IMG_0394.JPG" />
+                            </photos>
+                        </rsp>""")
+
+        photos_xml_3 = XML("""
+                        <rsp>
+                            <photos page="3" pages="3" perpage="1" total="3">
+                                <photo farm="3" id="2658720703" isfamily="0" isfriend="0" ispublic="1" lastupdate="1226520673" o_height="1500" o_width="1000" originalformat="jpg" originalsecret="yyyyyyyyyy" owner="25046991@N00" secret="yyyyyyyyyy" server="2126" title="16.jpg" />
+                            </photos>
+                        </rsp>""")
+        self.proxy.photos_recentlyUpdated.mock_returns_iter = iter([photos_xml_1, photos_xml_2, photos_xml_3])
+
+        self._mock_photos_http()
+
+        self.index.refresh()
+
+        self._check_photo_data(self.index.photos)
+        assert self.index.last_update == 1227123744
+
+    def test_refresh_duplicate(self):
+        """Non-destructive updates from Flickr"""
+
+        photos_xml = XML("""
+                        <rsp>
+                            <photos page="1" pages="1" perpage="100" total="3">
+                                <photo farm="4" id="2717638353" isfamily="0" isfriend="0" ispublic="1" lastupdate="1227123744" o_height="1024" o_width="1544" originalformat="jpg" originalsecret="1111111111" owner="25046991@N00" secret="xxxxxxxxxx" server="3071" title="87680027.JPG" />
+                                <photo farm="4" id="2740209939" isfamily="0" isfriend="0" ispublic="1" lastupdate="1222928301" o_height="2736" o_width="3648" originalformat="jpg" originalsecret="3333333333" owner="25046991@N00" secret="zzzzzzzzzz" server="3108" title="IMG_0394.JPG" />
+                                <photo farm="3" id="2658720703" isfamily="0" isfriend="0" ispublic="1" lastupdate="1226520673" o_height="1500" o_width="1000" originalformat="jpg" originalsecret="yyyyyyyyyy" owner="25046991@N00" secret="2222222222" server="2126" title="16.jpg" />
+                            </photos>
+                        </rsp>""")
+        self.proxy.photos_recentlyUpdated.mock_returns = photos_xml
+
+        # Test the initial refresh.
+
+        self._mock_photos_http()
+        self.index.refresh()
+        self._check_photo_data(self.index.photos)
+
+        # Make a noticable change that isn't reflected in the user data.
+
+        self.index.photos[2717638353].original_format = 'gif'
+        
+        # Refresh again...
+
+        self._mock_photos_http()
+        self.index.refresh()
+
+        assert self.index.photos[2717638353].original_format == 'gif'
