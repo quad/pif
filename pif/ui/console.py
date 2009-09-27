c0f7a48621c0fc287e3fdfe1bac42bd401a2174b
Scott Robinson <scott@quadhome.com>
2009-09-27 13:41:48 +0200

    Untested, but seemingly working, console client.

--- pif/ui/console.py
+++ pif/ui/console.py
@@ -1,55 +1,67 @@
 import logging
 import time
 
-from pif.ui import OPTIONS, common_run
+from pif.ui.shell import Shell
 
 LOG = logging.getLogger(__name__)
 
 
-def proxy_callback():
-    LOG.info('Waiting for authorization from Flickr...')
-    time.sleep(5)
+class ConsoleShell(Shell):
+    def _init_option_parser(self):
+        Shell._init_option_parser(self)
+
+        self.option_parser.add_option('-f', '--force', action='store_true',
+                                      help='force file(s) to be used')
+        self.option_parser.add_option('-m', '--mark', action='store_true',
+                                      help='mark files as uploaded')
 
 
-def progress_callback(state, meta):
-    msgs = {
-        'update': 'Loading updates from Flickr...',
-        'index': 'Indexing photos on Flickr...',
-    }
+    def proxy_callback(self, proxy, perms, token, frob):
+        LOG.info('Waiting for authorization from Flickr...')
+        LOG.info("(If nothing happens, open %s in your browser.)" % proxy.auth_url(perms, frob))
+        time.sleep(5)
 
-    a, b = meta
-    LOG.info("%s (%u / %u)" % (msgs[state], a, b))
 
+    def progress_callback(self, state, meta):
+        msgs = {
+            'photos': 'Loading updates from Flickr...',
+            'hashes': 'Indexing photos on Flickr...',
+        }
+
+        a, b = meta
+        LOG.info("%s (%u / %u)" % (msgs[state], a, b))
+
+
+    def run(self):
+        try:
+            index = self.make_index(self.proxy_callback, self.progress_callback)
+        except IOError:
+            return LOG.fatal("Couldn't connect to Flickr.")
+
+        for t, fn in ((index.type(fn), fn) for fn in self.filenames):
+            if t == 'new' or (t == 'old' and self.options.force):
+                if self.options.mark:
+                    index.ignore(fn)
+
+                    LOG.info("%s marked as already uploaded" % fn)
+                else:
+                    def _(progress, done):
+                        if done:
+                            print
+                            LOG.info("Uploaded %s" % fn)
+                        else:
+                            print "\rUploading %s... (%s%%)" % (fn, int(progress)),
+
+                    index.upload(fn, callback=_)
+            elif t == 'old':
+                LOG.info("%s already uploaded, skipping (use --force to upload)" % fn)
+            elif t == 'invalid':
+                LOG.warn("%s is invalid, skipping" % fn)
+            else:
+                LOG.debug("%s is type '%s'?" % (fn, t))
+
+        index.sync()
 
 def run():
-    OPTIONS.add_option('-m', '--mark', action='store_true',
-                       help='mark files as uploaded')
-    options, args = OPTIONS.parse_args()
-
-    indexes, images = common_run((options, args), proxy_callback, progress_callback)
-    file_index, flickr_index = indexes
-
-    if options.mark:
-        LOG.info('Marked as uploaded:')
-        for fn in images:
-            if not options.dry_run:
-                flickr_index.ignore(file_index[fn])
-
-            LOG.info("\t%s" % fn)
-    else:
-        for fn in images:
-            if options.dry_run:
-                LOG.info("Uploaded %s" % fn)
-            else:
-                def _(progress, done):
-                    if done:
-                        LOG.info("Uploaded %s" % fn)
-                    else:
-                        LOG.debug("Uploading %s... (%s%%)" % (fn, progress))
-
-                flickr_index.proxy.upload(filename=fn, callback=_)
-
-    file_index.close()
-
-    if not options.dry_run:
-        flickr_index.close()
+    s = ConsoleShell()
+    s.run()
